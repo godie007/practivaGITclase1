5a0bdfb15a4682ac14893d610974ee6d0d332fa0
godie007 <diegof.e3@gmail.com>
2021-12-03 18:51:58 -0500

    se agrega ejemplo de codigo en rama principal de trabajo

---
+++ Andres/main.py
@@ -0,0 +1,10 @@
+def saludar():
+    print("Hola mundo")
+
+# sum tow numbers when the first param is name and second param is age
+def sumar(name, age):
+    print("Hola " + name + " tu edad es " + str(age))
+
+saludar()
+# call function sumar with params name and age
+sumar("Andres", 32)
