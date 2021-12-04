f11693f20a33763c2c4e09efdc71129c2c75a3e8
godie007 <diegof.e3@gmail.com>
2021-12-03 19:04:47 -0500

    se agrega algoritmo de la serie de fibonacci

--- Andres/main.py
+++ Andres/main.py
@@ -8,3 +8,14 @@ def sumar(name, age):
 saludar()
 # call function sumar with params name and age
 sumar("Andres", 32)
+
+# create a recursive function that allows to calculate a number of the fibonacci series giving as a parameter an index and return the value
+def fibonacci(index):
+    if index == 0:
+        return 0
+    elif index == 1:
+        return 1
+    else:
+        return fibonacci(index - 1) + fibonacci(index - 2)
+# call the fibonacci function with a random argument
+print(fibonacci(4))
